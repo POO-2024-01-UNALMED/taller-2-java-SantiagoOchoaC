9227cc7807aa830a81a61ccf90eaacede7c55b58
SantiagoOchoaC <159485469+SantiagoOchoaC@users.noreply.github.com>
2024-02-17 21:41:44 -0500

    Clase Asiento

---
+++ src/test/java/test/Asiento.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Asiento {
+	String color;
+	int precio;
+	int registro;
+	
+	void cambiarColor(String color) {
+		if (color == "verde" || color=="amarillo" || color=="blanco" || color=="negro" || color=="rojo") {
+			this.color = color;
+		}
+	}
+}
